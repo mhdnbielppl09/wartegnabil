7cc48d9b6ff724b8d7370d1b4a385b19a0cb5f3a
muhammad nabiel <mhdnbielppl09@gmail.com>
2025-01-23 13:38:52 +0700

    Add files via upload

---
+++ restoran/js/script.js
@@ -0,0 +1,15 @@
+//Toggle class active
+const navbarNav = document.querySelector('.navbar-nav');
+// ketika hmburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => { 
+navbarNav.classList.toggle('active');
+};
+
+//klik di luar sidebar untuk menghingkan nav
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e){
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
